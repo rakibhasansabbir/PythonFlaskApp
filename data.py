57ae12c7dfa971400813781a5e223359ecefeda2
rakib <rakibhasansabbir@yahoo.com>
2017-09-26 19:24:24 +0600

    create basic Flask application

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article one',
+            'body':'Beautiful designs at up to 70% Off. Article offers stylish modern, mid-century and scandinavian furniture from world renowned designers at accessible prices.',
+            'author':'Rakib',
+            'createDate':'04-25-2017'
+        },
+        {
+            'id':2,
+            'title':'Article two',
+            'body':'Beautiful designs at up to 70% Off. Article offers stylish modern, mid-century and scandinavian furniture from world renowned designers at accessible prices.',
+            'author':'Shakib',
+            'createDate':'04-25-2017'
+        },
+        {
+            'id':3,
+            'title':'Article three',
+            'body':'Beautiful designs at up to 70% Off. Article offers stylish modern, mid-century and scandinavian furniture from world renowned designers at accessible prices.',
+            'author':'Tamim',
+            'createDate':'09-26-2017'
+        }
+
+    ]
+    return articles
